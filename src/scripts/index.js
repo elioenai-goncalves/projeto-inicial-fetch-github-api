2f3528b988e63351f8def66155c1af471752aad3
Elioenai Gonçalves <elioenai.andrade@hotmail.com>
2024-04-24 16:37:40 -0300

    Adicionando pedidos gerenciais API

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import { getUser } from "./services/user.js"
 import { getRepositories } from "./services/repositories.js"
+import { getEvents } from "./services/eventsUser.js"
 import { user } from "./objects/user.js"
 import { screen } from "./objects/screen.js"
 
@@ -37,9 +38,12 @@ async function getUserData(userName) {
     }
 
     const repositoriesResponse = await getRepositories(userName)
-    
+
+    const eventsResponse = await getEvents(userName)
+
     user.setInfo(userResponse)
     user.setRepositories(repositoriesResponse)
-    
+    user.setEvents(eventsResponse)
+
     screen.renderUser(user)
 }
--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -2,21 +2,53 @@ const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user){  
         this.userProfile.innerHTML = `<div class="info">
-                        <img scr="${user.avatar_url}" alt="Foto do perfil do usuário" />
+                        <img scr="${user.avatarUrl}" alt="Foto do perfil do usuário" />
                             <div class="data">
                                 <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                                <p>👥 Seguidores: ${user.followers ?? 'Não possui seguidores 😢'}</p>
+                                <p>👥 Seguindo: ${user.following ?? 'Não está seguindo ninguém 😢'}</p>
+                                <br>
                                 <p>${user.bio ?? 'Não possui bio cadastrada 😢'}</p>
                             </div>
                         </div>`
 
             let repositoriesItens = ''
-            user.repositories.forEach(repo => repositoriesItens += `<li><a hre="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+            user.repositories.forEach(repo => repositoriesItens += `<li>
+                                                                        <a href="${repo.html_url}" target="_blank">
+                                                                            ${repo.name}
+                                                                            <ul>
+                                                                                <li>🍴${repo.forks_count}</li>
+                                                                                <li>⭐${repo.stargazers_count}</li>
+                                                                                <li>👀${repo.watchers_count}</li>
+                                                                                <li>👨‍💻${repo.language}</li>
+                                                                            </ul>
+                                                                        </a>
+                                                                    </li>`)
 
             if(user.repositories.length > 0) {
-                this.userProfile.innerHTML += `<div class="repositories section">
-                                                <h2>Repositórios</h2>
-                                                <ul>${repositoriesItens}</ul>
-                                               </div>`
+                this.userProfile.innerHTML +=   `<div class="repositories section">
+                                                    <h2>Repositórios</h2>
+                                                    <ul>${repositoriesItens}</ul>
+                                                </div>`
+            }
+
+            let eventsList = ''
+
+            if(user.events){
+                user.events.forEach(event => {
+                    if(event.payload && event.payload.commits){
+                        event.payload.commits.forEach(commit => {
+                            eventsList += `<li><b>${event.repo.name}</b> - ${commit.message}</li>`
+                        })
+                    }
+                })
+            }
+
+            if (user.events.length > 0) {
+                this.userProfile.innerHTML +=   `<div class="events">
+                                                    <h2>Eventos</h2>
+                                                    <ul>${eventsList}</ul>
+                                                </div>`
             }
         },
 
