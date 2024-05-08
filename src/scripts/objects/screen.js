b63840e19a3f2cab857e7ffeb74251cbf3eb895e
Elioenai Gonçalves <elioenai.andrade@hotmail.com>
2024-05-08 17:02:50 -0300

    Adicionados operador de coalescencia nula em dados do repositorios

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,26 +1,26 @@
 const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user){  
-        this.userProfile.innerHTML = `<div class="info">
-                        <img scr="${user.avatarUrl}" alt="Foto do perfil do usuário" />
-                            <div class="data">
-                                <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
-                                <p>👥 Seguidores: ${user.followers ?? 'Não possui seguidores 😢'}</p>
-                                <p>👥 Seguindo: ${user.following ?? 'Não está seguindo ninguém 😢'}</p>
-                                <br>
-                                <p>${user.bio ?? 'Não possui bio cadastrada 😢'}</p>
-                            </div>
-                        </div>`
+        this.userProfile.innerHTML =`<div class="info">
+                                        <img scr="${user.avatarUrl}" alt="Foto do perfil do usuário" />
+                                        <div class="data">
+                                            <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                                            <p>👥 Seguidores: ${user.followers ?? 'Não possui seguidores 😢'}</p>
+                                            <p>👥 Seguindo: ${user.following ?? 'Não está seguindo ninguém 😢'}</p>
+                                            <br>
+                                            <p>${user.bio ?? 'Não possui bio cadastrada 😢'}</p>
+                                        </div>
+                                    </div>`
 
             let repositoriesItens = ''
             user.repositories.forEach(repo => repositoriesItens += `<li>
                                                                         <a href="${repo.html_url}" target="_blank">
                                                                             ${repo.name}
                                                                             <ul>
-                                                                                <li>🍴${repo.forks_count}</li>
-                                                                                <li>⭐${repo.stargazers_count}</li>
-                                                                                <li>👀${repo.watchers_count}</li>
-                                                                                <li>👨‍💻${repo.language}</li>
+                                                                                <li>🍴${repo.forks_count ?? "Sem forks"}</li>
+                                                                                <li>⭐${repo.stargazers_count ?? "Sem estrelas"}</li>
+                                                                                <li>👀${repo.watchers_count ?? "Sem seguidores"}</li>
+                                                                                <li>👨‍💻${repo.language ?? "Nenhuma linguagem informada"}</li>
                                                                             </ul>
                                                                         </a>
                                                                     </li>`)
